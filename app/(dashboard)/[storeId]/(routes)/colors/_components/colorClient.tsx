9a6dbc611c141eecdecf8a1c1fa12ce274220dae
Mohamedreda03 <mo03.reda@gmail.com>
2024-03-08 19:02:52 +0200

    f

--- app/(dashboard)/[storeId]/(routes)/colors/_components/colorClient.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/_components/colorClient.tsx
@@ -1,56 +1,16 @@
 "use client";
 
-// import Heading from "@/components/ui/Heading";
-// import { Button } from "@/components/ui/button";
-// import { Plus } from "lucide-react";
-// import { useParams, useRouter } from "next/navigation";
-// import { ColorColumn, columns } from "./columns";
-// import { Separator } from "@/components/ui/separator";
-// import { DataTable } from "@/components/ui/data-table";
-// import ApiList from "@/components/ui/api-list";
-
-// export default function colorClient({ data }: { data: ColorColumn[] }) {
-//   const router = useRouter();
-//   const params = useParams();
-
-//   return (
-//     <>
-//       <div className="flex items-center justify-between">
-//         <Heading
-//           title={`Colors (${data.length})`}
-//           description="Manage colors for your store"
-//         />
-//         <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
-//           <Plus className="h-4 w-4 mr-2" />
-//           Add New
-//         </Button>
-//       </div>
-//       <Separator className="my-4" />
-//       <DataTable columns={columns} data={data} searchKey="name" />
-//       <Heading title={`API`} description="API calls for Colors" />
-//       <div className="mt-4 flex flex-col gap-4">
-//         <ApiList entityName="colors" entityIdName="colorId" />
-//       </div>
-//     </>
-//   );
-// }
-
-import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
 import Heading from "@/components/ui/Heading";
 import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { ColorColumn, columns } from "./columns";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 import ApiList from "@/components/ui/api-list";
-import { ColorColumn, columns } from "./columns";
 
-export default function ColorClient({
-  data,
-  params,
-}: {
-  data: ColorColumn[];
-  params: { storeId: string };
-}) {
+export default function colorClient({ data }: { data: ColorColumn[] }) {
+  const params = useParams();
   const router = useRouter();
 
   return (
