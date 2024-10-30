60936194426d07d847b600f6b2fd0f4b34f27e3e
Mohamed Reda <mo03.reda@gmail.com>
2024-10-30 12:11:52 +0300

    fs

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -10,29 +10,25 @@ export default async function layout({
   children: React.ReactNode;
   params: { storeId: string };
 }) {
-  try {
-    const { userId } = auth();
-    if (!userId) {
-      redirect("/sign-in");
-    }
-
-    const store = await prisma.store.findFirst({
-      where: {
-        id: params.storeId,
-        userId,
-      },
-    });
-
-    if (!store) {
-      redirect("/");
-    }
-    return (
-      <>
-        <Navbar />
-        {children}
-      </>
-    );
-  } catch (error) {
-    console.log(error);
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
   }
+
+  const store = await prisma.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,27 +7,21 @@ export default async function SetupLayout({
 }: {
   children: React.ReactNode;
 }) {
-  try {
-    const { userId } = auth();
+  const { userId } = auth();
 
-    if (!userId) {
-      redirect("/sign-in");
-      return null;
-    }
-
-    const store = await prisma.store.findFirst({
-      where: {
-        userId,
-      },
-    });
-
-    if (store) {
-      redirect(`/${store.id}`);
-      return null;
-    }
-
-    return <>{children}</>;
-  } catch (error) {
-    console.log(error);
+  if (!userId) {
+    redirect("/sign-in");
   }
+
+  const store = await prisma.store.findFirst({
+    where: {
+      userId,
+    },
+  });
+
+  if (store) {
+    redirect(`/${store.id}`);
+  }
+
+  return <>{children}</>;
 }
